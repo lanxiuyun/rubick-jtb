c5cb56b065c99563054962fb100184dc24f2c56a
lanxiuyun <lanxiuyun310@gmail.com>
2025-11-12 14:49:24 +0800

    init (#1)
    * 1
    
    * pnpm-lock
    
    * delete
    
    * 初始化
    
    ---------
    
    Co-authored-by: 徐映白 <62323046+maplelost@users.noreply.github.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from "@/views/homeView.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: {
+      title: "首页",
+      description: "首页",
+    },
+  },
+];
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHashHistory(import.meta.env.BASE_URL), // 使用 hash 模式, 适配 rubick 的 file:// 协议;
+  routes,
+});
 
-export default router
+export default router;
