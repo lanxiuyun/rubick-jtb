f475d23f1ef89f4de7a320195f820c38e0b5bb3a
lanxiuyun <lanxiuyun310@gmail.com>
2025-11-12 17:05:49 +0800

    获取剪贴板历史

--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>Home</div>
+  <div v-for="record in records" :key="record.hash">
+    <div>{{ record.type }}</div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+interface ClipboardRecord {
+  hash: string;
+  type: "text" | "files" | "image";
+  value: any;
+  timestamp: string;
+}
+
+const records = ref<ClipboardRecord[]>([]);
+
+onMounted(async () => {
+  try {
+    records.value = await window.services.readAllRecords();
+
+    // 监听新增记录
+    window.services.listenAppendRecord((newRecord) => {
+      records.value = [newRecord, ...records.value];
+    });
+  } catch (err) {
+    console.error("读取剪贴板历史记录失败", err);
+  }
+});
+</script>
