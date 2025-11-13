e523cf1970e89324050a0af86d44a169458ff789
lanxiuyun <lanxiuyun310@gmail.com>
2025-11-13 14:02:47 +0800

    测试数据

--- src/tests/data.ts
+++ src/tests/data.ts
@@ -1,84 +1,30 @@
 import type { ClipboardRecord } from "@/types/services";
 
 const records: ClipboardRecord[] = [
-  {
-    type: "text",
-    value: "services",
-    timestamp: 1762937369791,
-    hash: "10cd395cf71c18328c863c08e78f3fd0",
-  },
-  {
-    type: "text",
-    value: "type",
-    timestamp: 1762937368781,
-    hash: "599dcce2998a6b40b1e38e8c6006cb0a",
-  },
-  {
-    type: "text",
-    value: "{}",
-    timestamp: 1762937367771,
-    hash: "6f1177909c3817299923551328043527",
-  },
-  {
-    type: "text",
-    value:
-      "https://docs.dlcv.com.cn/deploy/sdk/c_sdk#%E8%BF%94%E5%9B%9E%E7%BB%93%E6%9E%9C%E8%A7%A3%E6%9E%90",
-    timestamp: 1762936436227,
-    hash: "24ae7b8543cb052869f55b1d95359024",
-  },
-  {
-    type: "text",
-    value: "https://bbs2.dlcv.com.cn/t/topic/547",
-    timestamp: 1762936370813,
-    hash: "5dfeac35951c1f1f7c676522a30bcb2f",
-  },
-  {
-    type: "image",
-    size: 130771,
-    timestamp: 1762936289285,
-    hash: "cc7682a59441991976227413e2cc1113",
-    value:
-      "C:\\Users\\cyf\\AppData\\Roaming\\rubick\\clipboard-data\\1762935915639\\cc7682a59441991976227413e2cc1113",
-  },
-  {
-    type: "text",
-    value: "inference",
-    timestamp: 1762936222689,
-    hash: "00b8744e1a3f8bb9d2a7237048951b3b",
-  },
-  {
-    type: "text",
-    value:
-      "关于mask输出功能，免费接口暂不提供，而我们的DLL版本则包含此特性。或许您可以考虑使用DLL版本来满足这一需求。",
-    timestamp: 1762936148202,
-    hash: "9af8eff2903b4d787499d4f361462058",
-  },
-  {
-    type: "text",
-    value: "关于免费接口的功能，恕我直言，它似乎并不包含mask输出这一特性。",
-    timestamp: 1762936101760,
-    hash: "7483f401b7dbf348020d3fa89140576a",
-  },
-  {
-    type: "text",
-    value: "node_modules\r\n",
-    timestamp: 1762935998070,
-    hash: "2e5243fbad9b5b60464b4e0e54e3f30b",
-  },
-  {
-    type: "text",
-    value: "node_modules",
-    timestamp: 1762935915639,
-    hash: "3ec03583f8eaec275cb2183db769ff47",
-  },
-  {
-    type: "image",
-    size: 10296,
-    timestamp: 1762935421370,
-    hash: "aab398c1064500f1fdbb00bfc2f0670d",
-    value:
-      "C:\\Users\\cyf\\AppData\\Roaming\\rubick\\clipboard-data\\1762934802151\\aab398c1064500f1fdbb00bfc2f0670d",
-  },
+  // 【图片类型】
+  { type: "image", size: 130771, timestamp: 1762936289285, hash: "cc7682a59441991976227413e2cc1113", value: "C:\\Users\\cyf\\AppData\\Roaming\\rubick\\clipboard-data\\1762935915639\\cc7682a59441991976227413e2cc1113" },
+  { type: "image", size: 10296, timestamp: 1762935421370, hash: "aab398c1064500f1fdbb00bfc2f0670d", value: "C:\\Users\\cyf\\AppData\\Roaming\\rubick\\clipboard-data\\1762934802151\\aab398c1064500f1fdbb00bfc2f0670d" },
+
+  // 【文件类型 - 混合文件和文件夹】
+  { type: "files", value: [{ isFile: true, isDirectory: false, name: ".gitignore", path: "C:\\Users\\cyf\\Pictures\\.gitignore" }, { isFile: false, isDirectory: true, name: "Screenshots", path: "C:\\Users\\cyf\\Pictures\\Screenshots" }], timestamp: 1763011472062, hash: "cc0717c20de72119f3c6a2e35e400b47" },
+
+  // 【文件类型 - 多个图片文件】
+  { type: "files", value: [{ isFile: true, isDirectory: false, name: "lazyeat.png", path: "C:\\Users\\cyf\\Pictures\\lazyeat.png" }, { isFile: true, isDirectory: false, name: "images.jpg", path: "C:\\Users\\cyf\\Pictures\\images.jpg" }], timestamp: 1763011455970, hash: "c5ea0a41e4b25bbad7e13b63beaef6ef" },
+
+  // 【文件类型 - 单个图片文件】
+  { type: "files", value: [{ isFile: true, isDirectory: false, name: "images.jpg", path: "C:\\Users\\cyf\\Pictures\\images.jpg" }], timestamp: 1763011433834, hash: "9390e121af9127ba1ae5aceb63a3cf68" },
+
+  // 【文件类型 - 多个普通文件】
+  { type: "files", value: [{ isFile: true, isDirectory: false, name: "package.json", path: "C:\\work_sapce\\rubick-jtb\\package.json" }, { isFile: true, isDirectory: false, name: "index.html", path: "C:\\work_sapce\\rubick-jtb\\index.html" }, { isFile: true, isDirectory: false, name: "LICENSE", path: "C:\\work_sapce\\rubick-jtb\\LICENSE" }], timestamp: 1763011405664, hash: "6a74bb1ed217aa08ceee847f8b436cad" },
+
+  // 【文件类型 - 单个普通文件】
+  { type: "files", value: [{ isFile: true, isDirectory: false, name: "index.html", path: "C:\\work_sapce\\rubick-jtb\\index.html" }], timestamp: 1763011397602, hash: "3a64d00c7a34268a56e1145523d655c0" },
+
+  // 【文本类型 - 多行文本】
+  { type: "text", value: "这是多行文本\r\n这是多行文本\r\n这是多行文本", timestamp: 1763011326163, hash: "67103e6d8bd77f2f764015d1a923c51b" },
+
+  // 【文本类型 - 单行文本】
+  { type: "text", value: "关于mask输出功能，免费接口暂不提供，而我们的DLL版本则包含此特性。或许您可以考虑使用DLL版本来满足这一需求。", timestamp: 1763011323158, hash: "9af8eff2903b4d787499d4f361462058" },
 ];
 
 export const TEST_DATA = {
